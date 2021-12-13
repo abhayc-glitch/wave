ba62094a30a417854cd87ed8726ed2e03aa65d18
abhayc-glitch <78511893+abhayc-glitch@users.noreply.github.com>
2021-12-12 16:59:13 -0800

    Implemented email to user search system

--- server/src/main/java/com/backend/server/service/UserService.java
+++ server/src/main/java/com/backend/server/service/UserService.java
@@ -1,17 +1,27 @@
 package com.backend.server.service;
 
+import com.backend.server.repository.UserRepository;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class UserService implements UserDetailsService{
 
+    private final static String USER_NOT_FOUND_msg = "User with email %s is not found";
+    // Is the final immutable state always necessary
+    private UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+            .orElseThrow(() -> 
+                new UsernameNotFoundException((String.format(USER_NOT_FOUND_msg, email))));
     }
     
 }
