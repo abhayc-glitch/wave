610f18b8778a5217f01a29e2a7136052d90d17f1
abhayc-glitch <78511893+abhayc-glitch@users.noreply.github.com>
2021-12-20 09:11:30 -0800

    Implemented Registration Portal Service

--- server/src/main/java/com/backend/server/controller/RegistrationController.java
+++ server/src/main/java/com/backend/server/controller/RegistrationController.java
@@ -3,6 +3,8 @@ package com.backend.server.controller;
 import com.backend.server.dto.RegistrationRequest;
 import com.backend.server.service.RegistrationService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 // Used to create URL mapping
 @RequestMapping(path = "api/v1/registration")
 public class RegistrationController {
-
+    @Autowired
     private RegistrationService registrationService;
 
-
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
         return registrationService.register(request);
     }
--- server/src/main/java/com/backend/server/service/RegistrationService.java
+++ server/src/main/java/com/backend/server/service/RegistrationService.java
@@ -3,8 +3,10 @@ package com.backend.server.service;
 import com.backend.server.dto.RegistrationRequest;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Service
+@ResponseBody
 public class RegistrationService {
     public String register(RegistrationRequest request) {
         return "works";
